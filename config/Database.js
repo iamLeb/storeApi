67c617430b1c528aed53db7d2ae0b49a681a98b1
Caleb Joseph <calebbjoseph@gmail.com>
2024-11-14 13:40:35 -0600

    added update on db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log('MongoDB Connected'))
+    .then(() => console.log('Connected to database'))
     .catch((err) => console.log(`Error Connecting to Database`));
 
 module.exports = connect;
